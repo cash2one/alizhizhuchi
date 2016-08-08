68fb5b4a6bbe9993c4dcf2f6f578e7673e8cce4f
XianzhiChris <godwu2846@163.com>
2016-08-09 00:53:37 +0800

    增加论坛

---
+++ bbs/log/20160808/admin_login.php
@@ -0,0 +1,2 @@
+<?php exit;?>	2016-08-08 11:39:08	112.240.80.140	/bbs/admin/index-login.htm	0	login successed. uid:1
+<?php exit;?>	2016-08-08 21:05:55	39.75.146.243	/bbs/admin/index-login.htm	0	login successed. uid:1
